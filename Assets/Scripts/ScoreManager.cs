6270337a97c0796ed805b6c1dcd8775357d69f3e
Mohammad Alizadeh <mhalizadeh@hotmail.com>
2021-05-25 16:36:27 +0430

    Refactoring

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    #region Fields
+
+    [Header("Game Current Score References")]
+    [SerializeField] Text scoreTextUI;
+    
+    [Header("Game Over Scores References")]
+    [SerializeField] Text HighScoreText;
+    [SerializeField] Text CurrentScoreText;
+    
+    [Header("Score File Reference")]
+    [SerializeField] IntVariable scoreRef;
+    
+    const string highScore = "HighScore";
+
+    #endregion
+
+    void Start()
+    {
+        InitializeHighScoreSavedData();
+        UpdateInGameScoreUI();
+    }
+
+    void OnEnable()
+    {
+        BallInHoleDetection.OnBallEnterHole += UpdateInGameScore;
+        LandingGroundCheck.OnBallLandedOutsideHole += UpdateGameOverScores;
+    }
+
+    void UpdateInGameScore()
+    {
+        scoreRef.value++;
+        UpdateInGameScoreUI();
+    }
+    
+    void UpdateGameOverScores()
+    {
+        SetHighScoreValue();
+
+        HighScoreText.text = PlayerPrefs.GetInt(highScore).ToString();
+        CurrentScoreText.text = scoreRef.value.ToString();
+    }
+
+    private void UpdateInGameScoreUI()
+    {
+        scoreTextUI.text = scoreRef.value.ToString();
+    }
+    
+    private static void InitializeHighScoreSavedData()
+    {
+        if (!PlayerPrefs.HasKey(highScore))
+            PlayerPrefs.SetInt(highScore, 0);
+    }
+
+    private void SetHighScoreValue()
+    {
+        if (scoreRef.value > PlayerPrefs.GetInt(highScore))
+            PlayerPrefs.SetInt(highScore, scoreRef.value);
+    }
+
+    void OnDisable()
+    {
+        BallInHoleDetection.OnBallEnterHole -= UpdateInGameScore;
+        LandingGroundCheck.OnBallLandedOutsideHole -= UpdateGameOverScores;
+    }
+}
---
+++ Assets/Scripts/SetDefaultValues.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDefaultValues : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] IntVariable scoreRef;
+    [SerializeField] FloatVariable AimingSpeedRef;
+    [SerializeField] float DefaultAimingSpeed;
+
+    [Space]
+    [SerializeField] Text TutorialTextUI;
+    [SerializeField] string TutorialText;
+
+    [Space]
+    [SerializeField] BoolVariable EasyModeStateRef;
+
+    const string isItFirstTimePlayed = "isItFirstTimePlayed";
+
+    #endregion
+
+    void Awake()
+    {
+        InitializeFirstTimePlayedState();
+        ResetGamevaluesOnFirstTimePlayed();
+        ClearTutorialTextWhenItIsNotTheFirstTimePlayed();
+        UpdateFirstTimePlayedStateToFalse();
+    }
+
+    private static void UpdateFirstTimePlayedStateToFalse()
+    {
+        PlayerPrefs.SetInt(isItFirstTimePlayed, 0);
+    }
+
+    private static void UpdateFirstTimePlayedStateToTrue()
+    {
+        PlayerPrefs.SetInt(isItFirstTimePlayed, 1);
+    }
+
+    private void ClearTutorialTextWhenItIsNotTheFirstTimePlayed()
+    {
+        if (PlayerPrefs.GetInt(isItFirstTimePlayed) == 0)
+            TutorialTextUI.text = "";
+    }
+
+    private void ResetGamevaluesOnFirstTimePlayed()
+    {
+        if (PlayerPrefs.GetInt(isItFirstTimePlayed) == 1)
+            ResetGameValues();
+    }
+
+    void InitializeFirstTimePlayedState()
+    {
+        if (!PlayerPrefs.HasKey(isItFirstTimePlayed))
+            UpdateFirstTimePlayedStateToTrue();
+    }
+
+    public void ResetGameValues()
+    {
+        scoreRef.value = 0;
+        AimingSpeedRef.value = DefaultAimingSpeed;
+        TutorialTextUI.text = TutorialText;
+        EasyModeStateRef.value = true;
+    }
+
+    void OnApplicationQuit()
+    {
+        UpdateFirstTimePlayedStateToTrue();
+    }
+
+}
